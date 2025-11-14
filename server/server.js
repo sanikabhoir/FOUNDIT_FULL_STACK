0d48dfb9fa4fe1f9877bf5ee85e10d17dbc672fa
sanikabhoir <sanikasureshbhoir@gmail.com>
2025-11-14 14:24:51 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // --- Middleware ---
-app.use(cors({ origin: 'http://localhost:3000' }));
+// Allow all origins
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
